899a5990c46a8a26d7f09f5ce8c0c361c657216e
joaonunosantoscosta <joaonunosantoscosta@gmail.com>
2015-11-26 17:36:28 +0000

    teste Heroku

--- app/models/noticia.rb
+++ app/models/noticia.rb
@@ -1,5 +1,5 @@
 class Noticia < ActiveRecord::Base
-  validates :nome, presence: true, maximum: 50, message: "less than 50 if you don't mind"
+  validates :nome, presence: true, length: { maximum: 50 }, message: "less than 50 if you don't mind"
   validates :data, presence: true
   validates :descricao, presence: true
 end
